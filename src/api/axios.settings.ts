99db0f43b066cbbf51646cb0c747e7f026f76d47
gnomedevreact <143091438+gnomedevreact@users.noreply.github.com>
2023-10-06 23:00:15 +0300

    patch - api changes

--- src/api/axios.settings.ts
+++ src/api/axios.settings.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosRequest = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://chat-app-three-swart.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
